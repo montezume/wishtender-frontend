f3c66d6dbbe1860c9635bfa2f8a4014974f9251a
Dashiell BarkHuss <dashiellbarkhuss@gmail.com>
2020-09-14 21:39:44 -0500

    email set up

--- src/components/LandingPage/ThankYou.js
+++ src/components/LandingPage/ThankYou.js
@@ -26,10 +26,10 @@ function ThankYou() {
         win fast-track access by being a super supporter.&#128522;{" "}
       </p>
       <p>
-        Simply tweet why your excited about WishTender and tag two people who
+        Simply tweet why you're excited about WishTender and tag two people who
         might find value in WishTender. Don't forget to tag{" "}
-        <a href="https://twitter.com/">@WishTender</a> so we can find your
-        tweet! Here's a sample{" "}
+        <a href="https://twitter.com/wishtender">@WishTender</a> so we can find
+        your tweet! Here's a sample{" "}
         <a href="https://twitter.com/intent/tweet?text=I%27m%20excited%20to%20try%20@WishTender%20because%20%5Bfill%20out%20why%20you%27re%20excited%20for%20WishTender%5D.%20People%20that%20might%20also%20be%20interested%3A%20%5Btag%20some%20people%20here%5D">
           tweet
         </a>
