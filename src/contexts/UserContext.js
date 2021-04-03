ba600091e56b50e5e41955069649b03b5bc672c0
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-04-03 15:53:00 -0400

    trying to fix deploy

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -6,6 +6,7 @@ export const UserContext = createContext({
     let user = await fetch("/api/users/current", {
       credentials: "include",
     }).then((res) => {
+      console.log(res);
       if (res.status === 204) return Promise.resolve(null);
       return res.json();
     });
