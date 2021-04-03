17c9074600ad04e953ce68c1f4241c212cf7f2b4
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-04-03 12:32:45 -0400

    trying to fix deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // import React from 'react';
-import logo from "./logo.svg";
 // import './App.css';
 import "./myapp.css";
-import ProfileSection from "./components/wishlistpage/ProfileSection";
-import Header from "./components/nav/Header";
-import Wishlist from "./components/wishlistpage/Wishlist";
-import AppBar from "./components/nav/AppBar";
+
 import HomePage from "./components/HomePage";
-import CustomizedMenus from "./components/menu.js";
-import AddWish from "./components/wishlistpage/AddWish.js";
 
 import { createMuiTheme } from "@material-ui/core/styles";
-import purple from "@material-ui/core/colors/purple";
 import green from "@material-ui/core/colors/green";
 import { ThemeProvider } from "@material-ui/styles";
-import WishlistPage from "./components/wishlistpage/WishlistPage";
 // import './Styles/App.css';
 
 const theme = createMuiTheme({
@@ -69,13 +61,10 @@ function App(props) {
     //
     <ThemeProvider theme={theme}>
       <div className="App">
-        <CustomizedMenus />
         {/* <Header /> */}
         {/* <AppBar position="fixed"/> */}
         <Router>
           <Route path="/" exact component={HomePage} />
-          <Route path="/wishlist" render={() => <WishlistPage user={user} />} />
-          <Route path="/addwish" component={AddWish} />
         </Router>
       </div>
     </ThemeProvider>
