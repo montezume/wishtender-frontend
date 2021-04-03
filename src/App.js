aefa70081834a012d0d7b8852f6754f33887684f
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-04-03 11:49:48 -0400

    reverted app.js to last working deploy

--- src/App.js
+++ src/App.js
@@ -1,216 +1,80 @@
-import React, { useState, useEffect, useContext } from "react";
-import { UserContext } from "./contexts/UserContext";
-import { RouteContext } from "./contexts/RouteContext";
-import { CurrencyContext } from "./contexts/CurrencyContext";
-import { CountryContext } from "./contexts/CountryContext";
-import { LocaleContext } from "./contexts/LocaleContext";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import useTraceUpdate from "./scripts/useTraceUpdate";
-// import React from 'react';k
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// import React from 'react';
+import logo from "./logo.svg";
 // import './App.css';
-import {
-  clientCurrency,
-  parsedCookies,
-  chooseCurrency,
-} from "./scripts/helpers";
 import "./myapp.css";
+import ProfileSection from "./components/wishlistpage/ProfileSection";
+import Header from "./components/nav/Header";
+import Wishlist from "./components/wishlistpage/Wishlist";
+import AppBar from "./components/nav/AppBar";
 import HomePage from "./components/HomePage";
-import LandingPage from "./components/LandingPage/LandingPage";
-import ThankYou from "./components/LandingPage/ThankYou";
-import CustomizedMenus from "./components/nav/menu.js";
-import Cart from "./components/Cart/Cart.js";
-import Login from "./components/Login/Login.js";
-import Logout from "./components/nav/LogoutButton/LogoutButton.js";
-import WishTracker from "./components/WishTracker/WishTracker.js";
-import ConnectSetup from "./components/ConnectSetup/ConnectSetup";
-import ConfirmationEmail from "./components/ConfirmationEmail/ConfirmationEmail";
-import ConfirmEmail from "./components/ConfirmEmail/ConfirmEmail";
+import CustomizedMenus from "./components/menu.js";
+import AddWish from "./components/wishlistpage/AddWish.js";
 
+import { createMuiTheme } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
 import { ThemeProvider } from "@material-ui/styles";
-import WishlistPage from "./components/WishlistPage/WishlistPage";
-
-// import AddWish from "./components/wishlistpage/addwish1/AddWish.js";
-import SignUp from "./components/SignUp/SignUp";
-import SetUp from "./components/SetUp/SetUp";
+import WishlistPage from "./components/wishlistpage/WishlistPage";
 // import './Styles/App.css';
-import theme from "./theme";
-import CheckOutSuccess from "./components/CheckOutSuccess/CheckOutSuccess";
-// import WishForm from "./components/wishlistpage/AddWish/WishForm/WishForm";
-import ConnectSuccess from "./components/ConnectSuccess/ConnectSucess";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Nunito",
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
+  palette: {
+    primary: {
+      main: "#02bff2",
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 function App(props) {
-  const { getUser } = useContext(UserContext);
-  const [user, setUser] = useState();
-  const [isCurrentUsersProfile, setIsCurrentUsersProfile] = useState();
-  const cookies = parsedCookies();
-  useTraceUpdate(App.name, props, { user });
-
-  useEffect(() => {
-    getUser().then((user) => {
-      setUser(user);
-      if (!clientCurrency(user)) {
-        chooseCurrency(JSON.parse(parsedCookies().locale));
-      }
-    });
-  }, [getUser]);
-  const SwitchRoutes = (
-    <Switch>
-      <Route
-        path="/betathankyou"
-        exact
-        render={(props) => {
-          return (
-            <div>
-              <ThankYou />
-            </div>
-          );
-        }}
-      />
-
-      <Route
-        path="/demo"
-        exact
-        render={(props) => {
-          return (
-            <div>
-              <HomePage />
-            </div>
-          );
-        }}
-      />
-
-      <Route
-        path="/demo/wishlist"
-        render={(props) => {
-          return (
-            <div>
-              <WishlistPage user={user} />
-            </div>
-          );
-        }}
-      />
-      <Route
-        path="/"
-        exact
-        render={(props) => {
-          return (
-            <div>
-              <LandingPage />
-            </div>
-          );
-        }}
-      />
-      <Route path="/sign-up" exact>
-        <SignUp />
-      </Route>
-      <Route path="/wishlist-setup">
-        <SetUp />
-      </Route>
-      <Route
-        path="/order"
-        render={(props) => {
-          return <CheckOutSuccess />;
-        }}
-      />
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/logout">
-        <Logout />
-      </Route>
-      <Route
-        path="/cart"
-        render={(props) => {
-          return <Cart cart={props?.location?.props?.cart} />;
-        }}
-      />
-      <Route
-        path="/connect-setup"
-        exact
-        render={(props) => {
-          return (
-            <div>
-              <ConnectSetup />
-            </div>
-          );
-        }}
-      />
-      <Route
-        path="/connect-success"
-        exact
-        render={(props) => {
-          return (
-            <div>
-              <ConnectSuccess />
-            </div>
-          );
-        }}
-      />
-      <Route
-        path="/wish-tracker"
-        render={(props) => {
-          return <WishTracker />;
-        }}
-      />
-      <Route path="/confirmation-email">
-        <ConfirmationEmail />
-      </Route>
-      <Route path="/confirm-email">
-        <ConfirmEmail />
-      </Route>
-
-      <Route path="/:alias">
-        <WishlistPage />;
-      </Route>
-    </Switch>
-  );
-
-  const routesArray = SwitchRoutes.props.children.map(
-    (child) => child.props.path
-  );
-  routesArray.push(
-    routesArray.splice(
-      routesArray.findIndex((r) => r === "/"),
-      1
-    )[0]
-  );
-
+  const user = {
+    bannerPicUrl: "images/banner_pic.png",
+    profilePicUrl: "images/profile_pic.png",
+    displayName: "Brittany K.",
+    name: { first: "Brittany", last: "Kochover" },
+    profileMessage: "Love you guys <3",
+    wishlistItems: [
+      {
+        itemName: "YSL Stilettos",
+        price: "1000.00",
+        imageUrl: "images/heels.png",
+      },
+      {
+        itemName: "Reformation Dress",
+        price: "300.00",
+        imageUrl: "images/dress.png",
+      },
+      {
+        itemName: "Night Pallette",
+        price: "37.00",
+        imageUrl: "images/makeup.png",
+      },
+    ],
+  };
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
+        <CustomizedMenus />
+        {/* <Header /> */}
+        {/* <AppBar position="fixed"/> */}
         <Router>
-          {/* <Switch> */}
-          {/* needs to render differently if not accessed from redirect */}
-          {user !== undefined && (
-            <LocaleContext.Provider value={JSON.parse(cookies.locale).locale}>
-              <CountryContext.Provider
-                value={JSON.parse(cookies.locale).countryCode}
-              >
-                <CurrencyContext.Provider value={clientCurrency(user)}>
-                  <UserContext.Provider value={{ user, setUser, getUser }}>
-                    <RouteContext.Provider
-                      value={{
-                        isCurrentUsersProfile,
-                        setIsCurrentUsersProfile,
-                        allRoutes: routesArray,
-                      }}
-                    >
-                      <Switch>
-                        {/* <Route path="/" exact>
-                          <LandingPageMenu />
-                        </Route> */}
-                        <Route path="/">
-                          <CustomizedMenus />
-                        </Route>
-                      </Switch>
-                      {SwitchRoutes}
-                    </RouteContext.Provider>
-                  </UserContext.Provider>
-                </CurrencyContext.Provider>
-              </CountryContext.Provider>
-            </LocaleContext.Provider>
-          )}
-          {/* </Switch> */}
+          <Route path="/" exact component={HomePage} />
+          <Route path="/wishlist" render={() => <WishlistPage user={user} />} />
+          <Route path="/addwish" component={AddWish} />
         </Router>
       </div>
     </ThemeProvider>
