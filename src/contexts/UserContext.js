c138e546e46ca650ae2834ef0d887e1039bbdc9e
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-04-03 22:25:57 -0400

    log

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -6,6 +6,7 @@ export const UserContext = createContext({
     let user = await fetch("/api/users/current", {
       credentials: "include",
     }).then(async (res) => {
+      console.log(res);
       if (res.status === 204) return Promise.resolve(null);
       const p = await res.json();
       console.log("json", p);
