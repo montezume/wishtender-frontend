5bab989b109b14580c4ed3a50c8ea4536febd68a
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-05-06 17:30:43 -0500

    changed orders to work with new price data structure

--- src/components/WishTracker/GiftSm.js
+++ src/components/WishTracker/GiftSm.js
@@ -48,7 +48,7 @@ const Gift = ({ orderId, gift, setRefreshWishlist }) => {
       <TableCell className={classes.cell2}>
         QTY: {gift.qty}
         <br></br>
-        {gift.price}
+        {gift.price.original}
         <RemoveWish wish={gift.item._id}>
           <Button
             size="small"
