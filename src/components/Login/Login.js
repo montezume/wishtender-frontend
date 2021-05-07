256da28e0f0f6df0d624fdc4a04d6f5ffc7bb1f1
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-05-07 13:35:52 -0500

    desktop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,13 @@
 import React, { useState, useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import { TextField, Paper, Container, Box, Button } from "@material-ui/core";
 
 import { useForm } from "react-hook-form";
 import { Redirect } from "react-router-dom";
 export default function Login() {
   const [profile, setProfile] = useState(null);
   const { setUser, getUser } = useContext(UserContext);
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, error } = useForm();
   const onSubmit = (data) => {
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
@@ -33,22 +34,60 @@ export default function Login() {
       });
   };
   return (
-    <div>
-      {profile && <Redirect to={`/${profile}`} />}
+    <Box
+      display="flex"
+      style={{
+        height: "100%",
+        width: "100%",
+        backgroundImage:
+          "url('/images/background_graphic_pattern_Artboard.png')",
+        backgroundSize: "cover",
+      }}
+      height="100%"
+      alignItems="center"
+    >
+      <Container
+        maxWidth="xs"
+        component={Paper}
+        style={{ padding: "30px", opacity: ".97" }}
+      >
+        {profile && <Redirect to={`/${profile}`} />}
+        <form
+          style={{ display: "flex", flexDirection: "column", gap: "16px" }}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <TextField
+            name="email"
+            type="email"
+            inputRef={register({ required: "Email Required" })}
+            variant="outlined"
+            // inputProps={{
+            //   form: {
+            //     autocomplete: "off",
+            //   },
+            // }}
+          />
+          <TextField
+            name="password"
+            type="password"
+            inputRef={register({ required: "Password Required" })}
+            variant="outlined"
+            // autocomplete="new-password"
+          />
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          name="email"
-          ref={register({ required: "Email Required" })}
-          type="text"
-        />
-        <input
-          name="password"
-          ref={register({ required: "Password Required" })}
-          type="password"
-        />
-        <input type="submit" value="Login" />
-      </form>
-    </div>
+          <Button
+            color="primary"
+            variant="contained"
+            type="submit"
+            value="Login"
+          >
+            Login
+          </Button>
+          <Button color="primary" variant="outlined" value="Sign Up">
+            Sign Up
+          </Button>
+        </form>
+      </Container>
+    </Box>
   );
 }
