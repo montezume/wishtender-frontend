9e3c53f71830b7df38ad4b6048564e7ec22248b7
montezume <malcolmclaing@gmail.com>
2022-06-18 17:18:50 +0100

    fix: cookie rewrite to allow login

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -15,8 +15,26 @@ module.exports = function (app) {
           ? "http://localhost:4000"
           : "https://api.wishtender.com/",
       changeOrigin: true,
+      secure: false,
+      // must match your local dev env
+      cookieDomainRewrite: "localhost",
+      preserveHeaderKeyCase: true,
       headers: {
         Origin: "https://www.wishtender.com",
+      },
+      on: {
+        // remove secure mode from cookie
+        onProxyRes: (proxyRes, req, res) => {
+          const sc = proxyRes.headers["set-cookie"];
+          if (Array.isArray(sc)) {
+            proxyRes.headers["set-cookie"] = sc.map((sc) => {
+              return sc
+                .split(";")
+                .filter((v) => v.trim().toLowerCase() !== "secure")
+                .join("; ");
+            });
+          }
+        },
       },
     })
   );
